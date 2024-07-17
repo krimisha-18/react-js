c527662811414a10988b75996c2580bbab8e8204
krimisha <krimisha025@gmail.com>
2024-07-17 11:50:53 +0530

    pr

--- react-js/react-PR/PR - 13 google-authentication/src/Redux/Reduser/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from "redux"
-import UserReduser from "./UserReduser"
-const rootReduser = combineReducers({
-    user: UserReduser
-})
-export default rootReduser;
--- react-js/react-PR/PR - 13 google-authentication/src/Stote.js
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import rootReduser from "./Redux/Reduser";
-import {thunk} from "redux-thunk";
-
-const Store = createStore(rootReduser,applyMiddleware(thunk))
-export default Store;
